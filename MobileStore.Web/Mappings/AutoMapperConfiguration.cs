f1ec4c81d06f42c7f8d68a3f9680b2bdc471836c
QuocNP1 <quoc7533@gmai.com>
2019-05-04 15:49:14 +0700

    add identity

---
+++ MobileStore.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MobileStore.Model.Models;
+using MobileStore.Web.Models;
+
+namespace MobileStore.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Post, PostViewModel>();
+            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            Mapper.CreateMap<Tag, TagViewModel>();
+
+            Mapper.CreateMap<ProductCategory, ProductCategoryViewModel>();
+            Mapper.CreateMap<Product, ProductViewModel>();
+            Mapper.CreateMap<ProductTag, ProductTagViewModel>();
+            Mapper.CreateMap<Footer, FooterViewModel>();
+            Mapper.CreateMap<Slide, SlideViewModel>();
+            Mapper.CreateMap<Page, PageViewModel>();
+            Mapper.CreateMap<ContactDetail, ContactDetailViewModel>();
+
+            //Mapper.CreateMap<ApplicationGroup, ApplicationGroupViewModel>();
+            //Mapper.CreateMap<ApplicationRole, ApplicationRoleViewModel>();
+            //Mapper.CreateMap<ApplicationUser, ApplicationUserViewModel>();
+        }
+
+    }
+}
